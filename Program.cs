b3abcea1a7499380efbcc7dd90cfb68f874a2025
Eka051 <dianekaraharjo@gmail.com>
2025-05-11 22:58:54 +0700

    feat: add swagger to development

--- Program.cs
+++ Program.cs
@@ -79,7 +79,7 @@ using (var scope = app.Services.CreateScope())
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || true)
 {
     app.MapOpenApi();
     app.UseSwagger();
